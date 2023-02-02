34660e782579cacfbdf779e287c5fb7773b06373
neziedev <devnezie@gmail.com>
2023-02-02 06:09:27 -0500

    creates license

--- Exhibition.playground/Contents.swift
+++ Exhibition.playground/Contents.swift
@@ -1,12 +1,48 @@
 import UIKit
 
 /*
- Refer to the README.md file for all objectives and instructions.
+📖 - Refer to the README.md file for all objectives and instructions.
  
- Follow the order of I-IV while writing your code below each step.
+✅ - Follow the order of I-IV while writing your code below each step.
  
- You may copy and paste sub-steps from the README for clarity.
+💻 - You may copy and paste sub-steps from the README for clarity.
+
+🙏 - I have created a reference to some of the data needed to complete this project. But, I would still look at the website and create your own properties based on updates from the time this was logged. 
+
+
+Name: Central Park Zoo
+Location: New York City, NY
+
+Exhibits:
+
+Tisch Children's Zoo - Located on the eastern side of the zoo, this exhibit has a variety of small animals such as rabbits, guinea pigs, and chickens.
+Number of Animals: 20
+On Display: 20
+Behind the Scenes: 0
+Off-Exhibit: 0
+African Plains - Located on the western side of the zoo, this exhibit has a variety of animals such as zebras, giraffes, and ostriches.
+Number of Animals: 20
+On Display: 20
+Behind the Scenes: 0
+Off-Exhibit: 0
+Arctic Tundra - Located in the northern section of the zoo, this exhibit has a variety of Arctic animals such as polar bears, arctic foxes, and walruses.
+Number of Animals: 20
+On Display: 20
+Behind the Scenes: 0
+Off-Exhibit: 0
+Rainforest - Located in the southern section of the zoo, this exhibit has a variety of tropical animals such as monkeys, parrots, and jaguars.
+Number of Animals: 20
+On Display: 20
+Behind the Scenes: 0
+Off-Exhibit: 0
+Discovery Center - Located in the center of the zoo, this exhibit provides hands-on activities and experiences for visitors.
+Number of Animals: 0
+On Display: 0
+Behind the Scenes: 0
+Off-Exhibit: 0
 */
+
+
 
 
 // I. Data Structures
