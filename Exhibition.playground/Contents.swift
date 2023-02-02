4176212418a2866c488fe4c6efb211643bf55314
neziedev <devnezie@gmail.com>
2023-02-02 03:54:49 -0500

    adds readme steps to playground file

--- Exhibition.playground/Contents.swift
+++ Exhibition.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
+/*
+ Refer to the README.md file for all objectives and instructions.
+ 
+ Follow the order of I-IV while writing your code below each step.
+ 
+ You may copy and paste sub-steps from the README for clarity.
+*/
 
+
+// I. Data Structures
+
+
+// II. Adding Animals to the Zoo Exhibit
+
+
+// III. Printing Animal Information
+
+
+// IV. Printing On Display Exhibits
+
+
+// V. Sending Alert Messages
